857757ddadfe832f4f0caa4edafbf0a38cae2c82
AlexandreCourtin <alexandre.c.ext@padoa-group.com>
2020-10-22 15:09:44 +0200

    ff

---
+++ src/client/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import ClientComponent from "./ClientComponent";
+
+function App() {
+  const [loadClient, setLoadClient] = useState(true);
+  return (
+    <>
+      <button onClick={() => setLoadClient(prevState => !prevState)}>
+        STOP CLIENT
+      </button>
+      {loadClient ? <ClientComponent /> : null}
+    </>
+  );
+}
+
+export default App;
