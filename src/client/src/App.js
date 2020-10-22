8e6d2c830aa977df186086c547f22835c1d73794
AlexandreCourtin <alexandre.c.ext@padoa-group.com>
2020-10-22 17:05:16 +0200

    can attribute room name and player name to client

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,14 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import ClientComponent from "./ClientComponent";
 
 function App() {
-  const [loadClient, setLoadClient] = useState(true);
   return (
     <>
-      <button onClick={() => setLoadClient(prevState => !prevState)}>
-        STOP CLIENT
-      </button>
-      {loadClient ? <ClientComponent /> : null}
+      <ClientComponent />
     </>
   );
 }
--- src/client/src/ClientComponent.js
+++ src/client/src/ClientComponent.js
@@ -5,20 +5,40 @@ const ENDPOINT = "http://127.0.0.1:4001";
 export default function ClientComponent() {
   const [response, setResponse] = useState("");
 
+  let roomName = checkNameAndRoomValidity(window.location.hash.substring(
+    window.location.hash.lastIndexOf("#") + 1,
+    window.location.hash.lastIndexOf("[")
+  ));
+
+  let playerName = checkNameAndRoomValidity(window.location.hash.substring(
+    window.location.hash.lastIndexOf("[") + 1,
+    window.location.hash.lastIndexOf("]")
+  ));
+
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
     socket.on("FromAPI", data => {
       setResponse(data);
     });
 
-    // CLEAN UP THE EFFECT
     return () => socket.disconnect();
-    //
   }, []);
 
   return (
-    <p>
-      It's <time dateTime={response}>{response}</time>
-    </p>
+    <div>
+      <p>
+        It's <time dateTime={response}>{response}</time>
+      </p>
+      <p>room name: {roomName}</p>
+      <p>player name: {playerName}</p>
+    </div>
   );
+}
+
+function checkNameAndRoomValidity(s) {
+  if (s.includes("#") || s.includes("[") || s.includes("]")) {
+    return "error";
+  } else {
+    return s;
+  }
 }
