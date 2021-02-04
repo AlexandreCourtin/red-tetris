81bd30bd87f6495e325780f1f35559b9c9781500
AlexandreCourtin <acourtin@student.42.fr>
2021-02-04 18:55:51 +0100

    moved client logic to app js

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,12 +1,39 @@
 import React from "react";
+import socketIOClient from "socket.io-client";
 import ClientComponent from "./ClientComponent";
 
+const SERVERPATH = "http://127.0.0.1:4001";
+
+const socket = socketIOClient(SERVERPATH);
+
+const clientRoomName = checkNameAndRoomValidity(window.location.hash.substring(
+	window.location.hash.lastIndexOf("#") + 1,
+	window.location.hash.lastIndexOf("[")
+));
+
+const clientPlayerName = checkNameAndRoomValidity(window.location.hash.substring(
+	window.location.hash.lastIndexOf("[") + 1,
+	window.location.hash.lastIndexOf("]")
+));
+
+socket.emit('new player', clientPlayerName, clientRoomName);
+
+function checkNameAndRoomValidity(s) {
+	if (s == null) {
+		return "error";
+	} else if (s.includes("#") || s.includes("[") || s.includes("]")) {
+		return "error";
+	} else {
+		return s;
+	}
+}
+
 function App() {
-  return (
-    <>
-      <ClientComponent />
-    </>
-  );
+	return (
+		<>
+			<ClientComponent clientRoomName={clientRoomName} clientPlayerName={clientPlayerName}/>
+		</>
+	);
 }
 
 export default App;
--- src/client/src/ClientComponent.js
+++ src/client/src/ClientComponent.js
@@ -1,39 +1,13 @@
 import React, {Component} from "react";
-import socketIOClient from "socket.io-client";
-const SERVERPATH = "http://127.0.0.1:4001";
 
 class ClientComponent extends Component {
-	render() {
-		const socket = socketIOClient(SERVERPATH);
-
-		const clientRoomName = checkNameAndRoomValidity(window.location.hash.substring(
-			window.location.hash.lastIndexOf("#") + 1,
-			window.location.hash.lastIndexOf("[")
-		));
-	
-		const clientPlayerName = checkNameAndRoomValidity(window.location.hash.substring(
-			window.location.hash.lastIndexOf("[") + 1,
-			window.location.hash.lastIndexOf("]")
-		));
-	
-		socket.emit('new player', clientPlayerName, clientRoomName);
-	
+	render() {	
 		return (
 			<div>
-				<p>room name: {clientRoomName}</p>
-				<p>player name: {clientPlayerName}</p>
+				<p>room name: {this.props.clientRoomName}</p>
+				<p>player name: {this.props.clientPlayerName}</p>
 			</div>
 		);
-	}
-}
-
-function checkNameAndRoomValidity(s) {
-	if (s == null) {
-		return "error";
-	} else if (s.includes("#") || s.includes("[") || s.includes("]")) {
-		return "error";
-	} else {
-		return s;
 	}
 }
 
