e4a957f8dd6cd518d4318608c0c554361c03522d
AlexandreCourtin <acourtin@student.42.fr>
2021-02-04 18:46:37 +0100

    ClientComponent is now a class

--- src/client/src/ClientComponent.js
+++ src/client/src/ClientComponent.js
@@ -1,37 +1,40 @@
-import React, { useEffect, useState } from "react";
-import axios from 'axios';
+import React, {Component} from "react";
 import socketIOClient from "socket.io-client";
 const SERVERPATH = "http://127.0.0.1:4001";
 
-export default function ClientComponent() {
-  const socket = socketIOClient(SERVERPATH);
+class ClientComponent extends Component {
+	render() {
+		const socket = socketIOClient(SERVERPATH);
 
-  const clientRoomName = checkNameAndRoomValidity(window.location.hash.substring(
-    window.location.hash.lastIndexOf("#") + 1,
-    window.location.hash.lastIndexOf("[")
-  ));
-
-  const clientPlayerName = checkNameAndRoomValidity(window.location.hash.substring(
-    window.location.hash.lastIndexOf("[") + 1,
-    window.location.hash.lastIndexOf("]")
-  ));
-
-  socket.emit('new player', clientPlayerName, clientRoomName);
-
-  return (
-    <div>
-      <p>room name: {clientRoomName}</p>
-      <p>player name: {clientPlayerName}</p>
-    </div>
-  );
+		const clientRoomName = checkNameAndRoomValidity(window.location.hash.substring(
+			window.location.hash.lastIndexOf("#") + 1,
+			window.location.hash.lastIndexOf("[")
+		));
+	
+		const clientPlayerName = checkNameAndRoomValidity(window.location.hash.substring(
+			window.location.hash.lastIndexOf("[") + 1,
+			window.location.hash.lastIndexOf("]")
+		));
+	
+		socket.emit('new player', clientPlayerName, clientRoomName);
+	
+		return (
+			<div>
+				<p>room name: {clientRoomName}</p>
+				<p>player name: {clientPlayerName}</p>
+			</div>
+		);
+	}
 }
 
 function checkNameAndRoomValidity(s) {
-  if (s == null) {
-    return "error";
-  } else if (s.includes("#") || s.includes("[") || s.includes("]")) {
-    return "error";
-  } else {
-    return s;
-  }
+	if (s == null) {
+		return "error";
+	} else if (s.includes("#") || s.includes("[") || s.includes("]")) {
+		return "error";
+	} else {
+		return s;
+	}
 }
+
+export default ClientComponent;
