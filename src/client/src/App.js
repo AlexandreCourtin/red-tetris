57d885eec443f07b6af0dafe9634eb931807140f
AlexandreCourtin <acourtin@student.42.fr>
2021-02-15 12:23:25 +0100

    added inputs

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -33,6 +33,14 @@ if (clientRoomName.length > 0 && clientPlayerName.length > 0) {
 	inGame = true;
 }
 
+let command = {
+	up: false,
+	down: false,
+	left: false,
+	right: false,
+	space: false,
+}
+
 function App() {
 	const [serverState, setServerState] = useState('');
 
@@ -127,6 +135,75 @@ function App() {
 
 			playState = <TetrisGrid board={playerBoard} />;
 		}
+
+		// INPUTS
+		document.addEventListener('keydown', function(event) {
+			switch (event.key) {
+				default:
+					break;
+				case 'a':
+					command.left = true;
+					break;
+				case 'w':
+					command.up = true;
+					break;
+				case 'd':
+					command.right = true;
+					break;
+				case 's':
+					command.down = true;
+					break;
+				case 'ArrowLeft':
+					command.left = true;
+					break;
+				case 'ArrowUp':
+					command.up = true;
+					break;
+				case 'ArrowRight':
+					command.right = true;
+					break;
+				case 'ArrowDown':
+					command.down = true;
+					break;
+				case 'Space':
+					command.space = true;
+					break;
+			}
+		});
+
+		document.addEventListener('keyup', function(event) {
+			switch (event.key) {
+				default:
+					break;
+				case 'a':
+					command.left = false;
+					break;
+				case 'w':
+					command.up = false;
+					break;
+				case 'd':
+					command.right = false;
+					break;
+				case 's':
+					command.down = false;
+					break;
+				case 'ArrowLeft':
+					command.left = false;
+					break;
+				case 'ArrowUp':
+					command.up = false;
+					break;
+				case 'ArrowRight':
+					command.right = false;
+					break;
+				case 'ArrowDown':
+					command.down = false;
+					break;
+				case 'Space':
+					command.space = false;
+					break;
+			}
+		});
 
 		return (
 			<div>
