fe892ae540e5f477a5b4b1bed4294ff87c12fc52
Eliott SUITS <esuits@e1r12p16.42.fr>
2021-02-17 13:48:59 +0100

    on peut bouger les pieces avec les fleches

--- src/client/src/utils/input.js
+++ src/client/src/utils/input.js
@@ -17,6 +17,7 @@ export function getNextPieceNumber() {
 	return nextPieceNumber;
 }
 
+<<<<<<< HEAD
 export function getInputs() {
 	return commands;
 }
@@ -55,3 +56,133 @@ document.addEventListener('keydown', function(event) {
 	}
 });
 
+=======
+let tmpBoard = [];
+
+for (let i = 0 ; i < 10 ; i++) {
+	tmpBoard[i] = [];
+	for (let j = 0 ; j < 20 ; j++) {
+		if (clientBoard >= 0)
+			tmpBoard[i][j] = clientBoard[i][j];
+		else
+			tmpBoard[i][j] = 0;
+	}
+}
+
+setPiece(3);
+function setPiece(i) {
+	switch (i) {
+			default:
+				clientBoard[4][1] = -i;
+				clientBoard[4][2] = -i;
+				clientBoard[4][3] = -i;
+				clientBoard[4][4] = -i;
+				break;
+			case 2:
+				clientBoard[4][1] = -i;
+				clientBoard[5][1] = -i;
+				clientBoard[4][2] = -i;
+				clientBoard[5][2] = -i;
+				break;
+			case 3:
+				clientBoard[4][1] = -i;
+				clientBoard[5][1] = -i;
+				clientBoard[6][1] = -i;
+				clientBoard[5][2] = -i;
+				break;
+			case 4:
+				clientBoard[4][1] = -i;
+				clientBoard[5][1] = -i;
+				clientBoard[4][2] = -i;
+				clientBoard[3][2] = -i;
+				break;
+			case 5:
+				clientBoard[4][1] = -i;
+				clientBoard[5][1] = -i;
+				clientBoard[5][2] = -i;
+				clientBoard[6][2] = -i;
+				break;
+			case 6:
+				clientBoard[4][1] = -i;
+				clientBoard[4][2] = -i;
+				clientBoard[4][3] = -i;
+				clientBoard[3][3] = -i;
+				break;
+			case 7:
+				clientBoard[4][1] = -i;
+				clientBoard[4][2] = -i;
+				clientBoard[4][3] = -i;
+				clientBoard[5][3] = -i;
+				break;
+		}
+}
+
+let hasMoved = 0;
+let isTimeouting = false;
+export function getClientBoard() {
+	getInputs();
+
+	// TESTS MOVE SQUARE
+	for (let i = 0 ; i < 10 ; i++) {
+		for (let j = 0 ; j < 20 ; j++) {
+			if (hasMoved < 4) {
+				if (clientBoard[i][j] < 0 && j - 1 >= 0 && commands.up) {
+					tmpBoard[i][j - 1] = clientBoard[i][j];
+					console.log('up');
+					clientBoard[i][j] = 0;
+					hasMoved++;
+				}
+				else if (clientBoard[i][j] < 0 && j + 1 < 20 && commands.down) {
+					tmpBoard[i][j + 1] = clientBoard[i][j];
+					console.log('down');
+					clientBoard[i][j] = 0;
+					hasMoved++;
+				}
+				else if (clientBoard[i][j] < 0 && i - 1 >= 0 && commands.left) {
+					tmpBoard[i - 1][j] = clientBoard[i][j];
+					console.log('left');
+					clientBoard[i][j] = 0;
+					hasMoved++;
+				}
+				else if (clientBoard[i][j] < 0 && i + 1 < 10 && commands.right) {
+					tmpBoard[i + 1][j] = clientBoard[i][j];
+					console.log('right');
+					clientBoard[i][j] = 0;
+					hasMoved++;
+				}
+			}
+		}
+	}
+	if (hasMoved === 4)
+	{
+		// clientBoard = tmpBoard;
+		for (let i = 0 ; i < 10 ; i++) {
+			clientBoard[i] = [];
+			for (let j = 0 ; j < 20 ; j++) {
+				clientBoard[i][j] = tmpBoard[i][j];
+			}
+		}		
+		for (let i = 0 ; i < 10 ; i++) {
+			tmpBoard[i] = [];
+			for (let j = 0 ; j < 20 ; j++) {
+				if (clientBoard >= 0)
+					tmpBoard[i][j] = clientBoard[i][j];
+				else
+					tmpBoard[i][j] = 0;
+			}
+		}
+		hasMoved = 5;
+	}
+	if (isTimeouting === false) {
+		setTimeout(resetHasMoved, 100);
+		isTimeouting = true;
+	}
+
+	return clientBoard;
+}
+
+function resetHasMoved() {
+	hasMoved = 0;
+	isTimeouting = false;
+}
+>>>>>>> on peut bouger les pieces avec les fleches
